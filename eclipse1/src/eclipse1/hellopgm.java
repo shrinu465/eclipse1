b739724f7ec8fa1f220c59b98c25170c3ea4f05d
lenovo <lenovo@DESKTOP-2G00RPJ>
2019-10-09 21:09:08 +0530

    iam java

--- eclipse1/src/eclipse1/hellopgm.java
+++ eclipse1/src/eclipse1/hellopgm.java
@@ -4,7 +4,7 @@ public class hellopgm {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("helloworld");
+System.out.println("helloworld srinu");
 	}
 
 }
